d40ea696f4eb3a355cfa195609453b279d21305a
Ruben Quizon <quizon@fullsail.edu>
2013-10-03 23:41:30 -0500

    repopped

--- js/json.js
+++ js/json.js
@@ -19,6 +19,125 @@ var fillerData = {
 		"inumber": ["How Many", "1"],
 		"ihand": ["Carried By Hand", "No"],
 		"inotes": ["Notes", "Because Yes."]
+	},
+	"item4": {
+		"itype": ["Item Type", "Armor"],
+		"iname": ["Item Name", "Mithril Suit"],
+		"inumber": ["How Many", "1"],
+		"ihand": ["Carried By Hand", "No"],
+		"inotes": ["Notes", "It is said that this is impenetrable"]
+	},
+	"item5": {
+		"itype": ["Item Type", "Drink"],
+		"iname": ["Item Name", "Soda"],
+		"inumber": ["How Many", "3"],
+		"ihand": ["Carried By Hand", "Yes"],
+		"inotes": ["Notes", "..it's all you could find in the rush!"]
+	},
+	"item6": {
+		"itype": ["Item Type", "Tool"],
+		"iname": ["Item Name", "Sonic Screwdriver"],
+		"inumber": ["How Many", "1"],
+		"ihand": ["Carried By Hand", "Yes"],
+		"inotes": ["Notes", "The good Doctor uses it. That's all you need to know."]
+	},
+	"item7": {
+		"itype": ["Item Type", "Weapon"],
+		"iname": ["Item Name", "Knife"],
+		"inumber": ["How Many", "1"],
+		"ihand": ["Carried By Hand", "Yes"],
+		"inotes": ["Notes", "Now THAT is a knife!"]
+	},
+	"item8": {
+		"itype": ["Item Type", "Food"],
+		"iname": ["Item Name", "Bananas"],
+		"inumber": ["How Many", "4"],
+		"ihand": ["Carried By Hand", "Yes"],
+		"inotes": ["Notes", "Potassium is good for something, right?"]
+	},
+	"item9": {
+		"itype": ["Item Type", "Vehicle"],
+		"iname": ["Item Name", "Hummer"],
+		"inumber": ["How Many", "1"],
+		"ihand": ["Carried By Hand", "No"],
+		"inotes": ["Notes", "The military uses it, so should you."]
+	},
+	"item10": {
+		"itype": ["Item Type", "Armor"],
+		"iname": ["Item Name", "Gliding Suit"],
+		"inumber": ["How Many", "1"],
+		"ihand": ["Carried By Hand", "No"],
+		"inotes": ["Notes", "Sometimes you gotta have a quick escape plan"]
+	},
+	"item11": {
+		"itype": ["Item Type", "Drink"],
+		"iname": ["Item Name", "Water"],
+		"inumber": ["How Many", "5"],
+		"ihand": ["Carried By Hand", "Yes"],
+		"inotes": ["Notes", "Ah! Sweet H2O."]
+	},
+	"item12": {
+		"itype": ["Item Type", "Tool"],
+		"iname": ["Item Name", "Gerber"],
+		"inumber": ["How Many", "1"],
+		"ihand": ["Carried By Hand", "Yes"],
+		"inotes": ["Notes", "..the ultimate name in multi-tools"]
+	},
+	"item13": {
+		"itype": ["Item Type", "Weapon"],
+		"iname": ["Item Name", "Compound Bow"],
+		"inumber": ["How Many", "1"],
+		"ihand": ["Carried By Hand", "Yes"],
+		"inotes": ["Notes", "Best weapon to use in a crysis."]
+	},
+	"item14": {
+		"itype": ["Item Type", "Food"],
+		"iname": ["Item Name", "Cake"],
+		"inumber": ["How Many", "2"],
+		"ihand": ["Carried By Hand", "Yes"],
+		"inotes": ["Notes", "Don't eat it. It's a lie."]
+	},
+	"item15": {
+		"itype": ["Item Type", "Vehicle"],
+		"iname": ["Item Name", "Motocycle"],
+		"inumber": ["How Many", "1"],
+		"ihand": ["Carried By Hand", "No"],
+		"inotes": ["Notes", "When you gotta get somewhere fast..."]
+	},
+	"item16": {
+		"itype": ["Item Type", "Armor"],
+		"iname": ["Item Name", "Tight Pants"],
+		"inumber": ["How Many", "1"],
+		"ihand": ["Carried By Hand", "No"],
+		"inotes": ["Notes", "Don't bother trying to run."]
+	},
+	"item17": {
+		"itype": ["Item Type", "Drink"],
+		"iname": ["Item Name", "Alcohol"],
+		"inumber": ["How Many", "5"],
+		"ihand": ["Carried By Hand", "Yes"],
+		"inotes": ["Notes", "It helps soothe the pain."]
+	},
+	"item18": {
+		"itype": ["Item Type", "Tool"],
+		"iname": ["Item Name", "Crowbar"],
+		"inumber": ["How Many", "1"],
+		"ihand": ["Carried By Hand", "Yes"],
+		"inotes": ["Notes", "..also doubles as a weapon."]
+	},
+	"item19": {
+		"itype": ["Item Type", "Weapon"],
+		"iname": ["Item Name", "Shotgun"],
+		"inumber": ["How Many", "1"],
+		"ihand": ["Carried By Hand", "Yes"],
+		"inotes": ["Notes", "BANG! What's up with that thang?"]
+	},
+	"item20": {
+		"itype": ["Item Type", "Food"],
+		"iname": ["Item Name", "MRE"],
+		"inumber": ["How Many", "2"],
+		"ihand": ["Carried By Hand", "No"],
+		"inotes": ["Notes", "..lots of essentials in this, but not good coming out."]
 	}
 	
 }
