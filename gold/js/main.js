476089fd1e9f7ada13db96f166c2fe5e43245361
Ruben Quizon <quizon@fullsail.edu>
2013-10-18 22:41:12 -0500

    removed post method

--- gold/js/main.js
+++ gold/js/main.js
@@ -18,7 +18,7 @@ $('#additem').on('pageinit', function(){
 	});
 	
 	//any other code needed for addItem page goes here
-	
+
 });
 
 //The functions below can go inside or outside the pageinit function for the page in which it is needed.
